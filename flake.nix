2a234302670ad72c4f35cb9ce1b9afc5201aecff
Patrick Widmer <patrick.widmer@ergon.ch>
2026-04-11 17:26:08 +0200

    chore: init nix flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachDefaultSystem =
+        f:
+        lib.systems.flakeExposed
+        |> map (s: lib.mapAttrs (_: v: { ${s} = v; }) (f s))
+        |> lib.foldAttrs lib.mergeAttrs { };
+    in
+    {
+      overlays = {
+        default = final: prev: {
+
+        };
+      };
+    }
+    // eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            buildInputs = with pkgs; [ flatpak flatpak-builder ];
+          };
+        };
+
+        formatter = pkgs.nixfmt;
+      }
+    );
+}
